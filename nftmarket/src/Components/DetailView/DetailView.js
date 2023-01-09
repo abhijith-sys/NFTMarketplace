fed0e20ec6575b63903bec68dd5d84d60f302e70
inv-abhijith <abhijith.ani@innovaturelabs.com>
2023-01-09 09:18:29 +0530

    sfsf

--- nftmarket/src/Components/DetailView/DetailView.js
+++ nftmarket/src/Components/DetailView/DetailView.js
@@ -561,7 +561,7 @@ function DetailView(props) {
                                 <tr>
                                   <th scope="col">Price</th>
                                   <th scope="col">From</th>
-                                  <th scope="col">Action</th>
+                                  {/* <th scope="col">Action</th> */}
                                 </tr>
                                 {biddata.length === 0 ? (
                                   <td colSpan={3}>No bids </td>
@@ -588,23 +588,24 @@ function DetailView(props) {
                                             </button>
                                           </td>
                                         ) : (
-                                          <td style={{ color: "#a0a3a1" }}>
-                                            No action &nbsp;
-                                            <svg
-                                              xmlns="http://www.w3.org/2000/svg"
-                                              width="16"
-                                              height="16"
-                                              fill="currentColor"
-                                              className="bi bi-x-circle"
-                                              viewBox="0 0 16 16"
-                                            >
-                                              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                                              <path
-                                                d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5
-0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
-                                              />
-                                            </svg>
-                                          </td>
+//                                           <td style={{ color: "#a0a3a1" }}>
+//                                             No action &nbsp;
+//                                             <svg
+//                                               xmlns="http://www.w3.org/2000/svg"
+//                                               width="16"
+//                                               height="16"
+//                                               fill="currentColor"
+//                                               className="bi bi-x-circle"
+//                                               viewBox="0 0 16 16"
+//                                             >
+//                                               <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+//                                               <path
+//                                                 d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5
+// 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
+//                                               />
+//                                             </svg>
+//                                           </td>
+<></>
                                         )}
                                       </tr>
                                     );
@@ -639,13 +640,13 @@ function DetailView(props) {
                   Balance
                   <span className={detailcss.price}>&nbsp; {balance}</span>
                 </p>
-                <p>
+                {/* <p>
                   Floor price<span className={detailcss.price}>8.4 WETH</span>
                 </p>
                 <p>
                   Best offer
                   <span className={detailcss.price}>7.60094 WETH</span>
-                </p>
+                </p> */}
               </div>
               <br />
               <div className={detailcss.formGroup}>
